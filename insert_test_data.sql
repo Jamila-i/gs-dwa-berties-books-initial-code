ab8f54a77665b2d11803f677e3cf4e87f85a2091
Jamila <jibra001@gold.ac.uk>
2025-11-19 21:56:44 +0000

    Task 7: Added a default login

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,3 +3,7 @@
 USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
+
+INSERT INTO users (username, firstname, lastname, email, hashedPassword)
+VALUES
+('gold', 'Gold', 'User', 'gold@example.com', '$2b$10$w6i9.dMfH.C7pY3bvYknveEXkc7SMQIqCYwX0ZKKOlrdp/mi2Chj.');
